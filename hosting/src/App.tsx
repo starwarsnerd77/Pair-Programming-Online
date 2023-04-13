abbcd8020d390f64d577c2874e15750129f50c2b
bashton <14bashton@gmail.com>
2023-04-13 17:10:56 -0600

    add firebase config

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -4,15 +4,35 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth, signInWithRedirect } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuPjEO3_9gwMgCIHWKmzedMA9B4Ygpj94",
+  authDomain: "pair-programming-online.firebaseapp.com",
+  projectId: "pair-programming-online",
+  storageBucket: "pair-programming-online.appspot.com",
+  messagingSenderId: "691021243768",
+  appId: "1:691021243768:web:dec62d48f230d6c200548d",
+  measurementId: "G-QXYES6B1X2"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+
+const auth = getAuth();
+signInWithRedirect(auth, provider);
 
 function App() {
   const [count, setCount] = useState(0)
-  const provider = new GoogleAuthProvider();
-  
-  const auth = getAuth();
-  signInWithRedirect(auth, provider);
   
   return (
     <div className="App">
